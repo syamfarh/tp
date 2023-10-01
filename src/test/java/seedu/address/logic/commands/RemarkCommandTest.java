88ef01a586451d4bd8e64070535c39f2001b3645
syamfarh <e0939383@u.nus.edu>
2023-10-01 18:04:48 +0800

    Add EOF for RemarkCommandTest.java and RemarkTest.java

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -134,4 +134,5 @@ public class RemarkCommandTest {
         assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON,
                 new Remark(VALID_REMARK_BOB))));
     }
+    
 }
