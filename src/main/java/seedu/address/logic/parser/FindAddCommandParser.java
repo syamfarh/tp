357aca87bef17c760974b5d97612f7d2cd87eca6
limzhenwy <zwlim38@gmail.com>
2023-10-15 21:56:54 +0800

    Correct JavaDoc comments

--- src/main/java/seedu/address/logic/parser/FindAddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindAddCommandParser.java
@@ -9,13 +9,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.AddressContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments and creates a new FindAddCommand object
  */
 public class FindAddCommandParser implements Parser<FindAddCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the FindCommand
-     * and returns a FindCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the FindAddCommand
+     * and returns a FindAddCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindAddCommand parse(String args) throws ParseException {
