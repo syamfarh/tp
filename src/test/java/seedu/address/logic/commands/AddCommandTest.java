a9b9bf334067b5a1634e2bf8c19dfd451063681c
glenngnng <glennng12@gmail.com>
2023-10-15 20:19:44 +0800

    Fix ModelStub in AddCommandTest
    ModelStub now contains and overrrides storeDeletedPerson(deletedPerson)
    and getDeletedPerson() methods.

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -157,6 +157,16 @@ public class AddCommandTest {
         public void updateFilteredPersonList(Predicate<Person> predicate) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void storeDeletedPerson(Person deletedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Person getDeletedPerson() {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
