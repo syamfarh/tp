ad3e40069205cb95effd4e2d63519a42da783b84
glenngnng <glennng12@gmail.com>
2023-10-24 21:38:40 +0800

    Fix coding style for imports

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -23,8 +23,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.person.Person;
 import javafx.util.Pair;
+import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 
