6df496b3f8665d8383390dc3fd4234f3d4be6f59
Daphne789 <daphneshaine933@gmail.com>
2023-10-27 13:54:41 +0800

    Update the clone command to include risk profile

--- src/main/java/seedu/address/logic/commands/CloneCommand.java
+++ src/main/java/seedu/address/logic/commands/CloneCommand.java
@@ -97,7 +97,7 @@ public class CloneCommand extends Command {
         Name clonedName = new Name(nameWithoutNumbers + " " + updatedNumericSuffixStr);
         Person clonedPerson = new Person(clonedName, personToClone.getPhone(), personToClone.getEmail(),
                 personToClone.getOccupation(), personToClone.getAddress(), personToClone.getApptDate(),
-                personToClone.getTags());
+                personToClone.getRiskProfile(), personToClone.getTags());
         return clonedPerson;
     }
 }
