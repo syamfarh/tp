789bfc684fb771875022414165fd17cc69127086
syamfarh <e0939383@u.nus.edu>
2023-10-28 17:27:54 +0800

    Add calendarWindow UI

--- src/main/java/seedu/address/ui/CalendarWindow.java
+++ src/main/java/seedu/address/ui/CalendarWindow.java
@@ -4,15 +4,24 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.logging.Logger;
 
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.CalendarSource;
+import com.calendarfx.view.CalendarView;
+
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.util.CalendarUtil;
+import seedu.address.logic.Logic;
+import seedu.address.model.person.Person;
 
-import com.calendarfx.model.Calendar;
-import com.calendarfx.view.CalendarView;
-
-public class CalendarWindow extends UiPart<Stage>{
+/**
+ * Displays the Calendar.
+ * It contains appointment date of clients.
+ */
+public class CalendarWindow extends UiPart<Stage> {
 
     private static final String FXML = "CalendarWindow.fxml";
 
@@ -20,16 +29,27 @@ public class CalendarWindow extends UiPart<Stage>{
 
     private Calendar clients;
 
+    private Logic logic;
+    private Thread updateTimeThread;
+
     private CalendarView calendarView;
+
+    /**
+     * Creates a new CalendarWindow.
+     *
+     * @param root Stage to use as the root of the CalendarWindow.
+     */
     public CalendarWindow(Stage root) {
         super(FXML, root);
-        calendarView = new CalendarView(); // (1)
+        calendarView = new CalendarView();
         clients = new Calendar("Clients");
+        CalendarSource myCalendarSource = new CalendarSource("My Calendars");
+        myCalendarSource.getCalendars().addAll(clients);
+        calendarView.getCalendarSources().addAll(myCalendarSource);
         clients.setStyle(Calendar.Style.STYLE1);
-
         calendarView.setRequestedTime(LocalTime.now());
 
-        Thread updateTimeThread = new Thread("Calendar: Update Time Thread") {
+        updateTimeThread = new Thread("Calendar: Update Time Thread") {
             @Override
             public void run() {
                 while (true) {
@@ -52,24 +72,47 @@ public class CalendarWindow extends UiPart<Stage>{
         updateTimeThread.setPriority(Thread.MIN_PRIORITY);
         updateTimeThread.setDaemon(true);
         updateTimeThread.start();
-
+        root.setWidth(1000);
+        root.setHeight(600);
         Scene scene = new Scene(calendarView.getMonthPage());
         root.setScene(scene);
+        calendarView.getMonthPage().getMonthView().setDisable(true);
+        root.setResizable(true);
     }
 
     /**
      * Creates a new CalendarWindow.
      */
-    public CalendarWindow() {
+    public CalendarWindow(Logic logic) {
         this(new Stage());
+        this.logic = logic;
     }
 
+    /**
+     * Shows the calendar window.
+     * @throws IllegalStateException
+     *     <ul>
+     *         <li>
+     *             if this method is called on a thread other than the JavaFX Application Thread.
+     *         </li>
+     *         <li>
+     *             if this method is called during animation or layout processing.
+     *         </li>
+     *         <li>
+     *             if this method is called on the primary stage.
+     *         </li>
+     *         <li>
+     *             if {@code dialogStage} is already showing.
+     *         </li>
+     *     </ul>
+     */
     public void show() {
-        logger.fine("Showing Risk Assessment Questionnaire page.");
-        assert !isShowing() : "Questionnaire window is already showing";
+        logger.fine("Showing Calendar page.");
+        assert !isShowing() : "Calendar page is already showing";
+        update();
         getRoot().show();
         getRoot().centerOnScreen();
-        assert isShowing() : "Questionnaire window is not showing after show()";
+        assert isShowing() : "Calendar page is not showing after show()";
     }
 
     /**
@@ -91,6 +134,17 @@ public class CalendarWindow extends UiPart<Stage>{
      */
     public void focus() {
         getRoot().requestFocus();
+    }
+
+    /**
+     * Update the list of entries in the calendar.
+     */
+    public void update() {
+        ObservableList<Person> lists = logic.getFilteredPersonList();
+        clients.clear();
+        for (Person p : lists) {
+            clients.addEntry(CalendarUtil.createNewEntry(p));
+        }
     }
 
 }
--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -48,6 +48,9 @@ public class MainWindow extends UiPart<Stage> {
     private MenuItem questionnaireMenuItem;
 
     @FXML
+    private MenuItem calendarMenuItem;
+
+    @FXML
     private StackPane personListPanelPlaceholder;
 
     @FXML
@@ -73,7 +76,7 @@ public class MainWindow extends UiPart<Stage> {
 
         helpWindow = new HelpWindow();
         questionnaireWindow = new QuestionnaireWindow();
-        calendarWindow = new CalendarWindow();
+        calendarWindow = new CalendarWindow(logic);
     }
 
     public Stage getPrimaryStage() {
@@ -83,6 +86,7 @@ public class MainWindow extends UiPart<Stage> {
     private void setAccelerators() {
         setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
         setAccelerator(questionnaireMenuItem, KeyCombination.valueOf("F2"));
+        setAccelerator(calendarMenuItem, KeyCombination.valueOf("F3"));
     }
 
 
