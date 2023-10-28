f4a4ae6ffd9036affbafb99decb50d13d864051a
limzhenwy <zwlim38@gmail.com>
2023-10-28 14:45:13 +0800

    Add functionality for UFind command

--- src/main/java/seedu/address/logic/commands/UFindCommand.java
+++ src/main/java/seedu/address/logic/commands/UFindCommand.java
@@ -1,2 +1,15 @@
-package seedu.address.logic.commands;public class UFindCommand {
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class UFindCommand {
+
+    public static final String COMMAND_WORD = "ufind";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names or addresses contain "
+            + "any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: n/ or a/ KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " n/ alice bob charlie";
+
 }
--- src/main/java/seedu/address/logic/parser/UFindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UFindCommandParser.java
@@ -1,2 +1,41 @@
-package seedu.address.logic.parser;public class UFindParser {
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.UFindCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.stream.Stream;
+
+public class UFindCommandParser {
+
+    public Command parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ADDRESS);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME) && !arePrefixesPresent(argMultimap, PREFIX_ADDRESS)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UFindCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_ADDRESS);
+
+        if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            return new FindCommandParser().parse(argMultimap.getValue(PREFIX_NAME).get());
+        } else if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
+            return new FindAddCommandParser().parse(argMultimap.getValue(PREFIX_ADDRESS).get());
+        }
+        // should never reach this line
+        throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UFindCommand.MESSAGE_USAGE));
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
 }
