f259a3ade1585cd6c57a520f4db3e219ab8a4712
limzhenwy <e0957620@u.nus.edu>
2023-10-31 14:38:47 +0800

    Handle non-date inputs for appt searches

--- src/main/java/seedu/address/logic/parser/CalendarCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CalendarCommandParser.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.model.person.AppointmentDate.isValidCurrentDate;
+import static seedu.address.model.person.AppointmentDate.isValidFormat;
 
 import java.util.Arrays;
 
 import seedu.address.logic.commands.CalendarCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.AppointmentDate;
 import seedu.address.model.person.CalendarContainsKeywordsPredicate;
 
 /**
@@ -23,6 +26,13 @@ public class CalendarCommandParser implements Parser<CalendarCommand> {
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, CalendarCommand.MESSAGE_USAGE));
+        }
+
+        if (!isValidFormat(trimmedArgs + " 00:00")) {
+            throw new ParseException(AppointmentDate.MESSAGE_CONSTRAINTS_FORMAT);
+        }
+        if (!isValidCurrentDate(trimmedArgs + " 00:00")) {
+            throw new ParseException(AppointmentDate.MESSAGE_CONSTRAINTS_CURRENTDATE);
         }
 
         String[] calendarKeywords = trimmedArgs.split("\\s+");
