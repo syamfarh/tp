74cb5db3d5eeff044b868d06048ec0484e26da48
glenngnng <glennng12@gmail.com>
2023-11-03 02:13:03 +0800

    Fix checkstyle issue

--- src/test/java/seedu/address/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -7,6 +7,7 @@ import static seedu.address.logic.commands.RedoCommand.MESSAGE_REDO_SUCCESS;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
