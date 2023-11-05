f69bc954cc56f9ed7c9a3d715281a1050d927aa0
bhnuka <01bhanukaekanayake@gmail.com>
2023-11-05 10:48:43 +0800

    Update formatting

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -27,18 +27,18 @@ public class AddCommand extends Command {
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
-            + PREFIX_OCCUPATION + "OCCUPATION"
+            + PREFIX_OCCUPATION + "OCCUPATION "
             + PREFIX_ADDRESS + "ADDRESS "
-            + "[" + PREFIX_TAG + "TAG]...\n"
-            + "[" + PREFIX_APPOINTMENTDATE + "APPOINTMENT DATE]...\n"
+            + "[" + PREFIX_TAG + "TAG]... "
+            + "[" + PREFIX_APPOINTMENTDATE + "APPOINTMENT DATE]\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
-            + PREFIX_OCCUPATION + "SWE"
+            + PREFIX_OCCUPATION + "SWE "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney"
+            + PREFIX_TAG + "owesMoney "
             + PREFIX_APPOINTMENTDATE + "12-12-2023 08:30";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
